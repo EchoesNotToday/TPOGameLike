33cc60f1e526eec006a0ac8cc2cd582ef8c5eb0a
Eliez MAIGNÉ <emaign@gmail.com>
2020-03-26 11:02:07 +0100

    Partie_3 ADD OxygenGenerator impl

--- TPOGameLike/BO/ConcreteBuildings/OxygenGenerator.cs
+++ TPOGameLike/BO/ConcreteBuildings/OxygenGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BO.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,64 @@ namespace BO.ConcreteBuildings
         private Func<int?, int?> oxygenFunc = (int? x) => { return (200 * (x / 12)) + 20; };
         private Func<int?, int?> steelFunc = (int? x) => { return (1000 * (x / 8)) + 20; };
         private Func<int?, int?> uraniumFunc = (int? x) => { return (1500 * (x / 20)) + 20; };
+
+        public override List<Resource> TotalCost
+        {
+            get
+            {
+                List<Resource> res = new List<Resource>();
+                // Energy
+                Resource energy = new Resource() { Name = ResourcesConstants.RESOURCE_ENERGY, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(energyFunc, this.Level) };
+                res.Add(energy);
+
+                // Oxygen
+                Resource oxygen = new Resource() { Name = ResourcesConstants.RESOURCE_OXYGEN, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(oxygenFunc, this.Level) };
+                res.Add(oxygen);
+
+                // Steel
+                Resource steel = new Resource() { Name = ResourcesConstants.RESOURCE_STEEL, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(steelFunc, this.Level) };
+                res.Add(steel);
+
+                // Uranium
+                Resource uranium = new Resource() { Name = ResourcesConstants.RESOURCE_URANIUM, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(uraniumFunc, this.Level) };
+                res.Add(uranium);
+
+                return res;
+            }
+        }
+        public override List<Resource> NextCost
+        {
+            get
+            {
+                List<Resource> res = new List<Resource>();
+                // Energy
+                Resource energy = new Resource() { Name = ResourcesConstants.RESOURCE_ENERGY, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(energyFunc, this.Level +1) };
+                res.Add(energy);
+
+                // Oxygen
+                Resource oxygen = new Resource() { Name = ResourcesConstants.RESOURCE_OXYGEN, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(oxygenFunc, this.Level +1) };
+                res.Add(oxygen);
+
+                // Steel
+                Resource steel = new Resource() { Name = ResourcesConstants.RESOURCE_STEEL, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(steelFunc, this.Level +1) };
+                res.Add(steel);
+
+                // Uranium
+                Resource uranium = new Resource() { Name = ResourcesConstants.RESOURCE_URANIUM, LastUpdate = DateTime.Now, LastQuantity = ResourcesMathUtil.FactorialMath(uraniumFunc, this.Level +1) };
+                res.Add(uranium);
+
+                return res;
+            }
+        }
+        public override List<Resource> ResourceBySecond
+        {
+            get
+            {
+                List<Resource> res = new List<Resource>();
+                Resource oxygen = new Resource() { Name = ResourcesConstants.RESOURCE_OXYGEN, LastUpdate = DateTime.Now, LastQuantity = (20 * (this.Level / 2)) + 5 };
+                res.Add(oxygen);
+                return res;
+            }
+        }
     }
 }
